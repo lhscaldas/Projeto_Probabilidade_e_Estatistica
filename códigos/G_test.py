9f7d34b52a84f6c475d2cca966fa3f6c6fa3f712
lhscaldas <lhscaldas@gmail.com>
2024-12-30 10:15:08 -0300

    códigos da correlação e do G-test melhorados, relatórios dessas partes esboçado

--- códigos/G_test.py
+++ códigos/G_test.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-from scipy.stats import chi2
+from scipy.stats import chi2_contingency, chi2
 import json
 
-# Teste de hipótese G entre 2 conjuntos de dados
+# Função para calcular o G-test entre dois datasets usando scipy.stats.chi2_contingency
 def G_test(dataset1, dataset2, bins='auto', salvar=False):
     """
     Realiza o G-test entre dois conjuntos de dados.
@@ -11,62 +11,56 @@ def G_test(dataset1, dataset2, bins='auto', salvar=False):
     Parâmetros:
     - dataset1, dataset2: Arrays ou listas contendo os valores.
     - bins: Número de bins ou método para histogramas (padrão: 'auto').
+    - salvar: Flag para salvar resultados (não implementado na função atual).
 
     Retorna:
-    - G: Estatística do G-test.
-    - p_value: p-valor associado ao teste.
+    - resultados: Dicionário com estatística G, p-valor, bins e frequências observadas e esperadas.
     """
-    # Determinar bins comuns a partir dos dois datasets
+    # Determinar bins comuns para os dois datasets
     bins_edges = np.histogram_bin_edges(np.concatenate([dataset1, dataset2]), bins=bins)
-    
+
     # Calcular as frequências observadas para cada dataset
     freq1, _ = np.histogram(dataset1, bins=bins_edges)
     freq2, _ = np.histogram(dataset2, bins=bins_edges)
-    
-    # Total de observações em cada dataset
-    total1 = freq1.sum()
-    total2 = freq2.sum()
-    total_combined = total1 + total2
 
-    # Calcular as frequências esperadas para cada bin
-    freq_total = freq1 + freq2
-    freq_expected1 = freq_total * (total1 / total_combined)
-    freq_expected2 = freq_total * (total2 / total_combined)
+    # Criar matriz de contingência
+    contingency_table = np.array([freq1, freq2])
 
-    # Evitar divisão por zero e log de zero
+    # Calcular os valores esperados usando chi2_contingency
+    _, _, _, expected_freq = chi2_contingency(contingency_table, lambda_="log-likelihood")
+
+    # Prevenir divisão por zero e log de zero
     freq1 = np.maximum(freq1, 1e-10)
     freq2 = np.maximum(freq2, 1e-10)
-    freq_expected1 = np.maximum(freq_expected1, 1e-10)
-    freq_expected2 = np.maximum(freq_expected2, 1e-10)
+    expected_freq = np.maximum(expected_freq, 1e-10)
 
-    # Calcular a estatística do G-test
-    G1 = 2 * np.sum(freq1 * np.log(freq1 / freq_expected1))
-    G2 = 2 * np.sum(freq2 * np.log(freq2 / freq_expected2))
-    G = G1 + G2
+    # Calcular a estatística do G-test manualmente
+    G = 2 * np.sum(freq1 * np.log(freq1 / expected_freq[0]) + freq2 * np.log(freq2 / expected_freq[1]))
 
     # Calcular os graus de liberdade
-    df = len(bins_edges) - 1  # Número de bins - 1
+    df = len(bins_edges) - 2  # Número de bins - 1 para frequência - 1 adicional para soma restrita
 
     # Calcular o p-valor usando a distribuição qui-quadrado
     p_value = 1 - chi2.cdf(G, df=df)
 
-    # Agrupar resultados em um dicionário
+    # Retornar resultados em um dicionário estruturado
     resultados = {
         'G': G,
         'p_value': p_value,
+        'degrees_of_freedom': df,
         'bins_edges': bins_edges,
         'freq1': freq1,
         'freq2': freq2,
-        'freq_expected1': freq_expected1,
-        'freq_expected2': freq_expected2
+        'expected_freq': expected_freq
     }
 
     return resultados
 
 
+
 if __name__ == '__main__':
     # Salvar os resultados
-    salvar = False
+    salvar = True
 
     # Carregar os datasets
     dataset_1 = pd.read_csv('dados/dataset_1.csv')
